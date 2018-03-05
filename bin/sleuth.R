5c5aea284afcab7083fe7e1e54d44efd8f63976b
Ye Wang <yewang@dhcp-129-105-38-185.bmbcb.northwestern.edu>
2018-03-05 17:00:14 -0600

    Update the transcriptome version

--- bin/sleuth.R
+++ bin/sleuth.R
@@ -16,19 +16,24 @@ s2c <- s2c[order(s2c$condition), ]
 
 print(s2c)
 
-ensembl <- biomaRt::useEnsembl(biomart="ensembl", dataset = "celegans_gene_ensembl", version=79)
+ensembl <- biomaRt::useEnsembl(biomart = "ensembl", dataset = "celegans_gene_ensembl", version = 91)
 
 t2g <- biomaRt::getBM(attributes = c("ensembl_transcript_id", "ensembl_gene_id", "external_gene_name"), mart = ensembl)
 t2g <- dplyr::rename(t2g, target_id = ensembl_transcript_id, ens_gene = ensembl_gene_id, ext_gene = external_gene_name)
 
 # Load the kallisto processed data into the object
-so <- sleuth_prep(s2c, ~ condition, target_mapping = t2g, extra_bootstrap_summary = TRUE)
+so <- sleuth_prep(s2c, ~ condition, target_mapping = t2g, aggregation_column = 'ens_gene', read_bootstrap_tpm = TRUE, extra_bootstrap_summary = TRUE)
 
 # Estimate parameters for the sleuth response error measurement (full) model 
 so <- sleuth_fit(so)
 
 # Perform test
 so <- sleuth_wt(so,  'conditionN2')
+
+# Fit the full and reduced model and perform the test
+so <- sleuth_fit(so, ~condition, 'full')
+so <- sleuth_fit(so, ~1, 'reduced')
+so <- sleuth_lrt(so, 'reduced', 'full')
 
 gene_table <- sleuth_gene_table(so, test = "conditionN2", test_type = "wt")
 
